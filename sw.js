fd71ff6232b9638075f8d88ba4c01c55926ec2f6
Jason <93841589+anadskman@users.noreply.github.com>
2026-07-21 21:10:32 +0100

    Simplify service worker by removing cache handling
    Refactor service worker to remove caching logic and simplify event listeners.

--- sw.js
+++ sw.js
@@ -1,59 +1,19 @@
-const CACHE_NAME = "stocked-v2";
-
-const FILES = [
-    "/",
-    "/index.html",
-    "/recipes.html",
-    "/analytics.html",
-
-    "/css/styles.css",
-    "/css/variables.css",
-    "/css/recipes.css",
-    "/css/analytics.css",
-
-    "/js/database.js",
-    "/js/app.js",
-    "/js/recipes.js",
-    "/js/analytics.js",
-];
-
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches
-      .open(CACHE_NAME)
-
-      .then((cache) => {
-        return cache.addAll(FILES);
-      }),
-  );
-
-  self.skipWaiting();
+self.addEventListener("install", () => {
+    self.skipWaiting();
 });
 
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches
-      .keys()
 
-      .then((keys) => {
-        return Promise.all(
-          keys.map((key) => {
-            if (key !== CACHE_NAME) {
-              return caches.delete(key);
-            }
-          }),
-        );
-      }),
-  );
+self.addEventListener("activate", () => {
+
+    self.clients.claim();
+
 });
 
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches
-      .match(event.request)
 
-      .then((response) => {
-        return response || fetch(event.request);
-      }),
-  );
+self.addEventListener("fetch", event => {
+
+    event.respondWith(
+        fetch(event.request)
+    );
+
 });
